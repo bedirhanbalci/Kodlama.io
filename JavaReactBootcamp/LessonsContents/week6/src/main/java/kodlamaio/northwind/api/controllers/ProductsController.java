8502091ae5bddc70304175cbb37338567770a7b7
Bedirhan Balcı <bedirhanbalci@outlook.com>
2023-12-17 01:35:13 +0300

    week6 | Spring Boot in Enterprise Architecture

---
+++ JavaReactBootcamp/LessonsContents/week6/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -0,0 +1,23 @@
+package kodlamaio.northwind.api.controllers;
+
+import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.entities.concretes.Product;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@AllArgsConstructor
+public class ProductsController {
+
+    private final ProductService productService;
+
+    @GetMapping("/getAll")
+    public List<Product> getAll() {
+        return this.productService.getAll();
+    }
+}
---
+++ JavaReactBootcamp/LessonsContents/week6/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -0,0 +1,22 @@
+package kodlamaio.northwind.business.concretes;
+
+import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.dataAccess.abstracts.ProductDao;
+import kodlamaio.northwind.entities.concretes.Product;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class ProductManager implements ProductService {
+
+    private final ProductDao productDao;
+
+
+    @Override
+    public List<Product> getAll() {
+        return this.productDao.findAll();
+    }
+}
